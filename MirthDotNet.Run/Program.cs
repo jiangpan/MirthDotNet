f06453d8837017dcf8a57b61cea3335cadbe99b0
Joseph Daigle <joseph@cridion.com>
2014-02-12 21:44:57 -0500

    fixing compiler error, also an example of pruning errored messages

--- MirthDotNet.Run/Program.cs
+++ MirthDotNet.Run/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using MirthDotNet.Model;
 
@@ -11,46 +14,27 @@ namespace MirthDotNet
     {
         public static void Main(string[] args)
         {
-            // This will disable SSL certificate validation but useful for dev environments
-            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
-
-            var channelId = "d24f9ca7-d696-4ffd-b86e-484669213fa8";
-            var maxMessageId = client.GetMaxMessageId(channelId);
-            var filter = new MessageFilter()
-            {
-                MaxMessageId = maxMessageId,
-                Statuses = new List<string>() { "ERROR" },
-                EndDate = new DateTime(2014, 02, 05),
-            };
-            var messages = client.GetMessages(channelId, filter, includeContent: false, offset: 0, limit: 10);
-            var messageCount = client.GetMessageCount(channelId, filter);
-            var client = new Client("https://localhost:8443");
+            var client = new Client("https://127.0.0.1:8443");
             var loginStatus = client.Login("admin", "admin", "0.0.0");
-            //var serverId = client.GetServerId();
-            //var timeZone = client.GetServerTimezone();
-            //var status = client.GetStatus();
-            //var buildDate = client.GetBuildDate();
-            //var version = client.GetVersion();
-            //var tags = client.GetChannelTags();
             var channelStatusList = client.GetChannelStatusList().DashboardStatuses.ToArray();
-            var chan = channelStatusList.Where(x => x.Name.ToLower().Contains("blah")).Single();
-            //var serverSettings = client.GetServerSettings();
-            //var codeTemplates = client.GetCodeTemplate();
-            //var stats = channelStatusList.Select(x => client.GetStatistics(x.ChannelId)).ToArray();
-            //var log = client.ServerLog.GetServerLogs();
-            //// var connectionLog = client.DashboardConnectorStatus.GetConnectionInfoLogs();
-            //var connectionLog_channel = client.DashboardConnectorStatus.GetConnectionInfoLogs("POC - Inbound NEW");
-            var channelId = chan.ChannelId;
-            var connectorNames = client.GetConnectorNames(channelId);
-            var metadatacolumns = client.GetMetaDataColumns(channelId);
-            var maxmessageid = client.GetMaxMessageId(channelId);
-            var messages = client.GetMessages(channelId, new MessageFilter()
+
+            foreach (var channel in channelStatusList)
             {
-                MaxMessageId = maxmessageid,
-            });
-            var states = client.DashboardConnectorStatus.GetStates();
-            var _states = states.MirthMapItem.OrderBy(x => x.Key).ToArray();
-            var channelsDeployed = client.DashboardConnectorStatus.ChannelsDeployed();
+                var channelId = channel.ChannelId;
+                var maxMessageId = client.GetMaxMessageId(channelId);
+                var filter = new MessageFilter()
+                {
+                    MaxMessageId = maxMessageId,
+                    Statuses = new List<string>() { "ERROR" },
+                    EndDate = DateTime.Now.Date.AddDays(-7),
+                };
+                //var messages = client.GetMessages(channelId, filter, includeContent: false, offset: 0, limit: 10);
+                var messageCount = client.GetMessageCount(channelId, filter);
+                var sw = Stopwatch.StartNew();
+                //client.RemoveMessages(channelId, filter);
+                sw.Stop();
+                Console.WriteLine(channel.Name + " | " + messageCount + " | " + sw.Elapsed.TotalMilliseconds);
+            }
             client.Logout();
         }
     }
