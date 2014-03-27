e1d3f38623f3d0a752e3274e6491ec99d036c751
Joseph Daigle <joseph@cridion.com>
2014-03-27 11:10:29 -0400

    more sample code

--- MirthDotNet.Run/Program.cs
+++ MirthDotNet.Run/Program.cs
@@ -12,29 +12,91 @@ namespace MirthDotNet
 {
     public static class Program
     {
+        public const string ClientUrl = "https://127.0.0.1:8443";
+        public const string ClientUsername = "admin";
+        public const string ClientPassword = "admin";
+
+
         public static void Main(string[] args)
         {
-            var client = new Client("https://127.0.0.1:8443");
-            var loginStatus = client.Login("admin", "admin", "0.0.0");
-            var channelStatusList = client.GetChannelStatusList().DashboardStatuses.ToArray();
+            FindFailedToDeployConnectors();
+            PurgeERRORMessages();
+        }
 
-            foreach (var channel in channelStatusList)
+        public static string MaxOrPad(this string input, int totalWidth)
+        {
+            if (input.Length > totalWidth)
             {
-                var channelId = channel.ChannelId;
-                var maxMessageId = client.GetMaxMessageId(channelId);
-                var filter = new MessageFilter()
-                {
-                    MaxMessageId = maxMessageId,
-                    Statuses = new List<string>() { "ERROR" },
-                    EndDate = DateTime.Now.Date.AddDays(-7),
-                };
-                //var messages = client.GetMessages(channelId, filter, includeContent: false, offset: 0, limit: 10);
-                var messageCount = client.GetMessageCount(channelId, filter);
-                var sw = Stopwatch.StartNew();
-                //client.RemoveMessages(channelId, filter);
-                sw.Stop();
-                Console.WriteLine(channel.Name + " | " + messageCount + " | " + sw.Elapsed.TotalMilliseconds);
+                return input.Substring(0, totalWidth - 3) + "...";
             }
+            return input.PadRight(totalWidth);
+        }
+
+        public static void FindFailedToDeployConnectors()
+        {
+            var client = new Client(ClientUrl, timeout: int.MaxValue);
+            var loginStatus = client.Login(ClientUsername, ClientPassword, "0.0.0");
+            var channelStatusList = client.GetChannelStatusList().DashboardStatuses;
+            foreach (var item in channelStatusList.OrderBy(x => x.Name))
+            {
+                Console.Write("{0} deployed with {1} connectors. ", item.Name.MaxOrPad(30), item.ChildStatuses.DashboardStatuses.Count);
+                var channelId = item.ChannelId;
+                var channel = client.GetChannels(channelId).Channels.Single();
+                var connectors = channel.GetAllEnabledChannels();
+                if (connectors.Count != item.ChildStatuses.DashboardStatuses.Count)
+                {
+                    Console.Write("WARNING: Found {0} configured connectors!", connectors.Count);
+                    Console.Error.Write("WARNING: Found {0} configured connectors!", connectors.Count);
+                }
+                Console.WriteLine();
+            }
+            client.Logout();
+        }
+
+        public static void PurgeERRORMessages()
+        {
+            var client = new Client(ClientUrl, timeout: int.MaxValue);
+            var loginStatus = client.Login(ClientUsername, ClientPassword, "0.0.0");
+            var channelSummaryList = client.GetChannelSummary();
+
+            Console.WriteLine("Found " + channelSummaryList.Channels.Count + " channels...");
+            for (int i = 0; i < channelSummaryList.Channels.Count; i++)
+            {
+                try
+                {
+                    var channelId = channelSummaryList.Channels[i].Id;
+                    Console.Write((i) + " ");
+                    var maxMessageId = client.GetMaxMessageId(channelId);
+                    if (maxMessageId == 0)
+                    {
+                        Console.WriteLine();
+                        continue;
+                    }
+                    var filter = new MessageFilter()
+                    {
+                        MaxMessageId = maxMessageId,
+                        Statuses = new List<string>() { "ERROR" },
+                        EndDate = DateTime.Now.Date.AddDays(-7),
+                    };
+                    var messageCount = client.GetMessageCount(channelId, filter);
+                    var channel = client.GetChannels(channelId).Channels.Single();
+                    Console.Write("Found " + messageCount + " messages for " + channel.Name + "...");
+                    if (messageCount == 0)
+                    {
+                        Console.WriteLine();
+                        continue;
+                    }
+                    var sw = Stopwatch.StartNew();
+                    client.RemoveMessages(channelId, filter);
+                    sw.Stop();
+                    Console.WriteLine(" Deleted in " + sw.Elapsed.TotalSeconds.ToString("N2") + " sec.");
+                }
+                catch (Exception e)
+                {
+                    Console.Write(" ERROR: " + e.Message);
+                }
+            }
+
             client.Logout();
         }
     }
