6b6e3ac8440c6b390e1395235e55ece2c20ebfe2
Joseph Daigle <joseph@cridion.com>
2014-04-29 10:46:07 -0400

    adding ChannelProperties to loaded channel

--- MirthDotNet/Model/Channel.cs
+++ MirthDotNet/Model/Channel.cs
@@ -30,6 +30,9 @@ namespace MirthDotNet.Model
         [XmlArray("destinationConnectors"), XmlArrayItem("connector")]
         public Connector[] DestinationConnectors { get; set; }
 
+        [XmlElement("properties")]
+        public ChannelProperties Properties { get; set; }
+
         public ReadOnlyCollection<Connector> GetAllEnabledConnectors()
         {
             var list =new List<Connector>();
@@ -37,54 +40,5 @@ namespace MirthDotNet.Model
             list.AddRange(DestinationConnectors.Where(x => x.Enabled));
             return list.AsReadOnly();
         }
-
-        //[XmlElement("pruneMetaDataDays")]
-        //public int PruneMetaDataDays { get; set; }
-        //[XmlElement("archiveEnabled")]
-        //public bool ArchiveEnabled { get; set; }
-        /*
-         <id>352f2896-3292-48ec-85d6-d05b7a45b75c</id>
-    <nextMetaDataId>2</nextMetaDataId>
-    <name>BaptistPracticePlus - Test - Inbound</name>
-    <description>Port 9241</description>
-    <enabled>true</enabled>
-    <lastModified>
-      <time>1392244357134</time>
-      <timezone>America/New_York</timezone>
-    </lastModified>
-    <revision>6</revision>
-         <properties version="3.0.1">
-      <clearGlobalChannelMap>true</clearGlobalChannelMap>
-      <messageStorageMode>PRODUCTION</messageStorageMode>
-      <encryptData>false</encryptData>
-      <removeContentOnCompletion>false</removeContentOnCompletion>
-      <removeAttachmentsOnCompletion>false</removeAttachmentsOnCompletion>
-      <initialState>STARTED</initialState>
-      <storeAttachments>false</storeAttachments>
-      <tags class="linked-hash-set">
-        <string>Practice Plus</string>
-        <string>Inbound</string>
-        <string>Test</string>
-      </tags>
-      <metaDataColumns>
-        <metaDataColumn>
-          <name>SOURCE</name>
-          <type>STRING</type>
-          <mappingName>mirth_source</mappingName>
-        </metaDataColumn>
-        <metaDataColumn>
-          <name>TYPE</name>
-          <type>STRING</type>
-          <mappingName>mirth_type</mappingName>
-        </metaDataColumn>
-      </metaDataColumns>
-      <attachmentProperties>
-        <type>None</type>
-        <properties/>
-      </attachmentProperties>
-      <pruneMetaDataDays>7</pruneMetaDataDays>
-      <archiveEnabled>true</archiveEnabled>
-    </properties>
-         */
     }
 }
---
+++ MirthDotNet/Model/ChannelProperties.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace MirthDotNet.Model
+{
+    [Serializable]
+    public class ChannelProperties
+    {
+        [XmlElement("initialState")]
+        public DeployedState InitialState { get; set; }
+        [XmlElement("clearGlobalChannelMap")]
+        public string ClearGlobalChannelMap { get; set; }
+
+        [XmlElement("messageStorageMode")]
+        public string MessageStorageMode { get; set; }
+        [XmlElement("storeAttachments")]
+        public string StoreAttachments { get; set; }
+        [XmlElement("removeContentOnCompletion")]
+        public string RemoveContentOnCompletion { get; set; }
+        [XmlElement("removeAttachmentsOnCompletion")]
+        public string RemoveAttachmentsOnCompletion { get; set; }
+        [XmlElement("encryptData")]
+        public string EncryptData { get; set; }
+
+        [XmlElement("pruneMetaDataDays")]
+        public string PruneMetaDataDays { get; set; }
+        [XmlElement("archiveEnabled")]
+        public string ArchiveEnabled { get; set; }
+
+        [XmlElement("metaDataColumns")]
+        public MetaDataColumnList MetaDataColumns { get; set; }
+
+        [XmlArray("tags"), XmlArrayItem("string")]
+        public List<string> Tags { get; set; }
+    }
+}
